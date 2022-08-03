26c2ba35e22a17e51bf4638da039c8ebbb816bfe
EmoTab <mhmmd.imran02@gmail.com>
2022-08-03 12:16:54 +0600

    started array loops

---
+++ array-loops.js
@@ -0,0 +1,7 @@
+//target: display every elements of an array
+
+var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41];
+for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+}
