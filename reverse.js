1cf29b3b3f6e0ee84696448539f2d879547d218b
EmoTab <mhmmd.imran02@gmail.com>
2022-08-03 18:07:39 +0600

    added summary

---
+++ reverse.js
@@ -0,0 +1,37 @@
+//UNDERSTAND FOR AND WHILE LOOP IN A REVERSE WAY
+
+// var num = 1;
+// while (num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+//revers way above loop
+// var num = 10;
+// while (num >= 1){
+//     console.log(num);
+//     num--;
+// }
+
+//Now in for loop
+
+// for (var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+//revers way above for loop
+
+// for (var i = 10; i >= 1; i--){
+//     console.log(i);
+// }
+//Isosceles or not
+var side1 = 9;
+var side2 = 8;
+var side3 = 9;
+
+if(side1 == side2 || side1 == side3 || side2 == side3){
+    console.log('isosceles');
+}
+else{
+    console.log('not isosceles');
+}
---
+++ summary.js
@@ -0,0 +1,9 @@
+/**
+ * 1. while loop
+ * 2. for loop
+ * 3. break
+ * 4. continue
+ * 5. find even and odd using loop
+ * 6. 1-10 and 10-1 see using loop
+ * 7. Practices
+ */
